6a7d74655de51cfde0069e7c025a049af1049fb4
valentynsuliaiev <sulaev.1996@gmail.com>
2024-05-09 07:30:52 +0300

    add 10 th

--- app/models/pipeline.py
+++ app/models/pipeline.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel, Field, StrictStr
 from enum import Enum
 
 class Models(str, Enum):
-    #KNearest_Neighbors="KNearest_Neighbors",
+    KNearest_Neighbors="KNearest_Neighbors",
     Logistic_Regression="Logistic_Regression",
     Naive_Bayes="Naive_Bayes",
     Support_Vector_Classifier="Support_Vector_Classifier"
@@ -11,5 +11,5 @@ class Models(str, Enum):
 
 class PipelineReq(BaseModel):
     input: Any = Field(..., title="input", description="Input words", example="The quick brown fox jumps over the lazy dog")
-    models: Models = Field(..., title="models", description="Model name `Logistic_Regression` `Naive_Bayes` `Support_Vector_Classifier`")
+    models: Models = Field(..., title="models", description="Model name `KNearest_Neighbors` `Logistic_Regression` `Naive_Bayes` `Support_Vector_Classifier`")
     options: Optional[list] = Field(..., title="options", description="Preprocessing options `use_tokenize` | `use_stopwords` | `use_stemming` | `use_lemmatize` | `use_lowers`", example=["use_tokenize", "use_stopwords", "use_stemming" ,"use_lemmatize" , "use_lowers"])
